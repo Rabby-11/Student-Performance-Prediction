08fa35576537ccb51babfabd9514bb670d000c9a
Rabby-11 <ziaulhaquerabby@gmail.com>
2025-08-16 11:56:27 +0600

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ st.title("🎓 Student Performance Prediction App")
 st.write("Enter student data to predict performance (Pass/Fail).")
 
 # Collect user inputs
-Attendance = st.number_input("Attendance (%)", min_value=0, max_value=100, value=0)
-Assignment_Score = st.number_input("Assignment Score", min_value=0, max_value=100, value=0)
-Quiz_Score = st.number_input("Quiz Score", min_value=0, max_value=100, value=0)
-Study_Hours_Per_Week = st.number_input("Study Hours per Week", min_value=0, max_value=80, value=0)
-Internal_Assessment = st.number_input("Internal Assessment Score", min_value=0, max_value=100, value=0)
-Participation_Score = st.number_input("Participation Score", min_value=0, max_value=100, value=0)
-Project_Score = st.number_input("Project Score", min_value=0, max_value=100, value=0)
+Attendance = st.number_input("Attendance (%)", min_value=0, max_value=100, value=12)
+Assignment_Score = st.number_input("Assignment Score", min_value=0, max_value=100, value=12)
+Quiz_Score = st.number_input("Quiz Score", min_value=0, max_value=100, value=13)
+Study_Hours_Per_Week = st.number_input("Study Hours per Week", min_value=0, max_value=80, value=14)
+Internal_Assessment = st.number_input("Internal Assessment Score", min_value=0, max_value=100, value=15)
+Participation_Score = st.number_input("Participation Score", min_value=0, max_value=100, value=16)
+Project_Score = st.number_input("Project Score", min_value=0, max_value=100, value=17)
 Exam_Anxiety_Level = st.number_input("Exam Anxiety Level (1-10)", min_value=1, max_value=10, value=5)
 
 if st.button("Predict Performance"):
